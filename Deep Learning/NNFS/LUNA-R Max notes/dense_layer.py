cd1f16d7feba789c624b96635c2eb21e50fa6afc
Venchislaw <venchislavcodes@gmail.com>
2024-07-23 10:26:23 +0300

    Whaaa

--- Deep Learning/NNFS/LUNA-R Max notes/dense_layer.py
+++ Deep Learning/NNFS/LUNA-R Max notes/dense_layer.py
@@ -1,10 +1,22 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import nnfs
+from nnfs.datasets import spiral_data
 
 
 class Dense:
     def __init__(self, n_inputs, n_neurons, weights=None) -> None:
-        self.weights = np.random.rand((n_inputs, n_neurons))
+        if not weights:
+            self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        else:
+            self.weights = weights
         self.biases = np.zeros((1, n_neurons))
 
     def forward(self, inputs):
-        pass
+        self.z = np.dot(inputs, self.weights) + self.biases
+        return self.z
+
+
+X, y = spiral_data(samples=100, classes=2)
+z = Dense(2, 5).forward(X)
+print(z[:5], z.shape)
